7d66b166850a187a19a637eafe1c2fa582d05d5a
Chaphasilor <ppp.u@web.de>
2023-06-29 13:47:44 +0200

    use tokio async interval instead of busy waiting
    - reduces CPU usage significantly

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::{
     error::Error,
-    fmt,
-    net::{SocketAddr, UdpSocket},
-    time::{Instant, Duration}, alloc::System,
+    net::{SocketAddr},
+    time::{Duration}, sync::Arc,
 };
 use rand;
 use clap::Parser;
 use serde_json::json;
+use tokio::{net::UdpSocket, time};
+use std::sync::Mutex;
+use futures::{future};
 
 #[derive(serde::Deserialize, Debug, Clone)]
 pub struct Config {
@@ -14,10 +16,11 @@ pub struct Config {
     pub flow_name: String,
     pub target_ip: String,
     pub target_port: u16,
-    pub outbound_port: u16,
+    pub outbound_port_data: u16,
+    pub outbound_port_acks: Option<u16>,
     pub inbound_port: u16,
-    pub interval: Option<u128>,
-    pub inbound_poll_interval: Option<u128>,
+    pub interval: Option<u64>,
+    pub inbound_poll_interval: Option<u64>,
 }
 
 /// A simple application emulating a physical input node
@@ -36,24 +39,28 @@ struct Args {
     /// The initial target port
     #[arg(short='p', long)]
     target_port: Option<u16>,
-    /// The outgoing port
+    /// The outgoing port for sending data
     #[arg(short, long)]
-    outbound_port: Option<u16>,
+    outbound_port_data: Option<u16>,
+    /// The outgoing port for sending ACKs
+    #[arg(long, default_value = "0")]
+    outbound_port_acks: u16,
     /// The incoming port
     #[arg(short='i', long)]
     inbound_port: Option<u16>,
     /// data interval (ms)
     #[arg(long, default_value = "1000")]
-    interval: u128,
+    interval: u64,
     /// inbound poll interval (ms)
     #[arg(long, default_value = "10")]
-    inbound_poll_interval: u128,
+    inbound_poll_interval: u64,
     /// config file
     #[arg(short, long)]
     config: Option<String>,
 }
 
-pub fn main() -> () {
+#[tokio::main]
+async fn main() -> () {
 
     let args = Args::parse();
     let config: Config;
@@ -62,6 +69,12 @@ pub fn main() -> () {
         Some(config_path) => {
             match load_config(config_path.as_str()).as_mut() {
                 Ok(loaded_config) => {
+                    match loaded_config.outbound_port_acks {
+                        None => {
+                            loaded_config.outbound_port_acks = Some(args.outbound_port_acks);
+                        },
+                        _ => {},
+                    }
                     match loaded_config.interval {
                         None => {
                             loaded_config.interval = Some(args.interval);
@@ -91,7 +104,8 @@ pub fn main() -> () {
                 flow_name: args.flow.expect("Argument `flow` is required unless a config file is specified!").clone(),
                 target_ip: args.target_ip.expect("Argument `target_ip` is required unless a config file is specified!").clone(),
                 target_port: args.target_port.expect("Argument `target_port` is required unless a config file is specified!"),
-                outbound_port: args.outbound_port.expect("Argument `outbound_port` is required unless a config file is specified!"),
+                outbound_port_data: args.outbound_port_data.expect("Argument `outbound_port_data` is required unless a config file is specified!"),
+                outbound_port_acks: Some(args.outbound_port_acks),
                 inbound_port: args.inbound_port.expect("Argument `inbound_port` is required unless a config file is specified!"),
                 interval: Some(args.interval),
                 inbound_poll_interval: Some(args.inbound_poll_interval),
@@ -101,26 +115,56 @@ pub fn main() -> () {
 
     println!("Starting input node for flow '{}' @ area '{}'", config.flow_name, config.area);
 
-    let mut target = SocketAddr::from(format!("{}:{}", config.target_ip, config.target_port).parse::<SocketAddr>().expect("No valid target address given. Use format: <ip>:<port>"));
+    let target: Arc<Mutex<SocketAddr>> = Arc::new(Mutex::new(SocketAddr::from(format!("{}:{}", config.target_ip, config.target_port).parse::<SocketAddr>().expect("No valid target address given. Use format: <ip>:<port>"))));
 
-    let outbound_socket = UdpSocket::bind(format!("0.0.0.0:{}", config.outbound_port)).expect("Couldn't bind outbound socket");
+    let outbound_socket_data = UdpSocket::bind(format!("0.0.0.0:{}", config.outbound_port_data)).await.expect("Couldn't bind outbound socket");
+    let outbound_socket_acks = UdpSocket::bind(format!("0.0.0.0:{}", config.outbound_port_acks.unwrap())).await.expect("Couldn't bind outbound socket");
+    let inbound_socket = UdpSocket::bind(format!("0.0.0.0:{}", config.inbound_port)).await.expect("Couldn't bind inbound socket");
+    // let timeout = Duration::from_millis(10);
+    // inbound_socket.set_read_timeout(timeout.into()).expect("Couldn't set socket timeout");
 
-    let inbound_socket = UdpSocket::bind(format!("0.0.0.0:{}", config.inbound_port)).expect("Couldn't bind inbound socket");
-    let timeout = Duration::from_millis(10);
-    inbound_socket.set_read_timeout(timeout.into()).expect("Couldn't set socket timeout");
-
-
-    let mut timer_generate_data = Instant::now();
-    let mut timer_check_incoming = timer_generate_data.clone();
 
     let mut buf = [0; 1024];
 
-    loop {
+    let mut tasks: Vec<tokio::task::JoinHandle<()>> = vec![];
 
-        if timer_check_incoming.elapsed().as_millis() > config.inbound_poll_interval.unwrap() {
+    let target_data_clone = target.clone();
+
+    // send input data
+    tasks.push(tokio::spawn(async move {
+
+        let mut interval = time::interval(Duration::from_millis(config.interval.unwrap()));
+
+        loop {
+
+            interval.tick().await;
+
+            let data = generate_input_data();
+
+            let json = json!({
+                "message": data.to_string(),
+                "meta": {
+                    "flow_name": config.flow_name,
+                    "execution_area": config.area
+                }
+            });
+            
+            let target = target_data_clone.lock().unwrap().clone();
+            println!("Sending data to {}: {}", target, data);
+            outbound_socket_data.send_to(&json.to_string().as_bytes(), target).await.expect("Couldn't send data");
+
+        }
+    }));
+
+    let target_updates_clone = target.clone();
+
+    // receive target updates
+    tasks.push(tokio::spawn(async move {
+
+        loop {
 
             // check socket for incoming data
-            if let Ok((message_length, src)) = inbound_socket.recv_from(&mut buf) {
+            if let Ok((message_length, src)) = inbound_socket.recv_from(&mut buf).await {
                 // convert to string
                 let message = String::from_utf8(buf[..message_length].into()).expect("Couldn't convert to String");
                 println!("Received data from {}: {}", src, message);
@@ -137,10 +181,12 @@ pub fn main() -> () {
 
                         let new_target_address_string = format!("{}:{}", json["target"].as_str().expect("No target ip given"), new_target_port);
                         let new_target_address = SocketAddr::from(new_target_address_string.parse::<SocketAddr>().expect(format!("Target not updated because target address was invalid: {}", new_target_address_string).as_str()));
-                        target = new_target_address;
+                        {
+                            let mut target = target_updates_clone.lock().unwrap();
+                            *target = new_target_address;
+                        }
                         
                         // acknowledge
-                        let data = generate_input_data();
                         let json = json!({
                             "type": "updateTarget",
                             "success": true,
@@ -148,7 +194,7 @@ pub fn main() -> () {
                         println!("Sending ACK to {}: {}", src, json.to_string());
                         // send 10 times to "make sure" it arrives
                         for _ in 0..10 {
-                            outbound_socket.send_to(&json.to_string().as_bytes(), src).expect("Couldn't send ACK");
+                            outbound_socket_acks.send_to(&json.to_string().as_bytes(), src).await.expect("Couldn't send ACK");
                         }
                     }
                 } 
@@ -158,30 +204,11 @@ pub fn main() -> () {
                 // println!("No data received")
             }
 
-            // reset timer
-            timer_check_incoming = Instant::now();
         }
-        
-        if timer_generate_data.elapsed().as_millis() > config.interval.unwrap() {
 
-            let data = generate_input_data();
+    }));
 
-            let json = json!({
-                "message": data.to_string(),
-                "meta": {
-                    "flow_name": config.flow_name,
-                    "execution_area": config.area
-                }
-            });
-            
-            println!("Sending data to {}: {}", target, data);
-            outbound_socket.send_to(&json.to_string().as_bytes(), target).expect("Couldn't send data");
-
-            // reset timer
-            timer_generate_data = Instant::now();
-        }
-        
-    }
+    future::join_all(tasks).await;
 
 }
 
